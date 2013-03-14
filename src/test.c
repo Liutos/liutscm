550b549ddeebababc26ee63fabd55819ded916e7
LiuTao <mat.liutos@gmail.com>
2013-03-14 15:44:35 +0800

    Test the read, eval and print in batch way
    Use the GNU library function `fmemopen' to make `FILE *' type objects
    for testing

---
+++ src/test.c
@@ -0,0 +1,28 @@
+/*
+ * test.c
+ *
+ * Simple tests
+ *
+ * Copyright (C) 2013-03-14 liutos <mat.liutos@gmail.com>
+ */
+#include <stdio.h>
+#include <string.h>
+#include "write.h"
+#include "eval.h"
+#include "read.h"
+
+int main(int argc, char *argv[])
+{
+  char *cases[] = {
+    "123",
+    "-123",
+  };
+  for (int i = 0; i < sizeof(cases) / sizeof(char *); i++) {
+    FILE *stream = fmemopen(cases[i], strlen(cases[i]), "r");
+    printf(">> %s\n=> ", cases[i]);
+    write_object(eval_object(read_object(stream)));
+    putchar('\n');
+    fclose(stream);
+  }
+  return 0;
+}
