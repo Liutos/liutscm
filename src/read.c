0c231cab4eca59067a955689440dc84e56ec0647
LiuTao <mat.liutos@gmail.com>
2013-03-27 00:22:58 +0800

    Add the primitive functions include bitwise `and', `or' and `not'

--- src/read.c
+++ src/read.c
@@ -52,7 +52,7 @@ lisp_object_t read_float(int integer, lisp_object_t port) {
   return make_flonum(integer + number * 1.0 / i);
 }
 
-lisp_object_t read_number(char c, int sign, lisp_object_t port) {
+sexp read_number(char c, int sign, sexp port) {
   int number = c - '0';
   int digit;
   while (isdigit(digit = port_read_char(port))) {
@@ -95,7 +95,7 @@ lisp_object_t read_character(lisp_object_t port) {
   }
 }
 
-lisp_object_t read_string(lisp_object_t port) {
+sexp read_string(sexp port) {
   static char buffer[BUFFER_SIZE];
   int i = 0;
   int c = port_read_char(port);
@@ -109,8 +109,8 @@ lisp_object_t read_string(lisp_object_t port) {
   return make_string(str);
 }
 
-lisp_object_t read_pair(lisp_object_t port) {
-  lisp_object_t object = read_object(port);
+sexp read_pair(sexp port) {
+  sexp object = read_object(port);
   if (is_eof(object)) {
     fprintf(stderr, "Incomplete literal for list\n");
     exit(1);
@@ -118,19 +118,20 @@ lisp_object_t read_pair(lisp_object_t port) {
   if (is_close_object(object))
     return make_empty_list();
   if (is_dot_object(object)) {
-    lisp_object_t o1 = read_object(port);
-    lisp_object_t o2 = read_object(port);
+    sexp o1 = read_object(port);
+    sexp o2 = read_object(port);
     if (is_close_object(o2))
       return o1;
     else {
-      fprintf(stderr, "More than one objects after '.' at line %d\n", in_port_linum(port));
+      fprintf(stderr, "More than one objects after '.' at line %d\n",
+              in_port_linum(port));
       exit(1);
     }
   } else
     return make_pair(object, read_pair(port));
 }
 
-lisp_object_t read_symbol(char init, lisp_object_t port) {
+sexp read_symbol(char init, sexp port) {
   static char buffer[BUFFER_SIZE];
   int i = 1;
   int c = port_read_char(port);
@@ -146,17 +147,17 @@ lisp_object_t read_symbol(char init, lisp_object_t port) {
   return find_or_create_symbol(name);
 }
 
-lisp_object_t read_vector(lisp_object_t port) {
-  lisp_object_t list = read_pair(port);
+sexp read_vector(sexp port) {
+  sexp list = read_pair(port);
   int length = pair_length(list);
-  lisp_object_t vector = make_vector(length);
+  sexp vector = make_vector(length);
   for (int i = 0; !is_null(list); i++, list = pair_cdr(list)) {
     vector_data_at(vector, i) = pair_car(list);
   }
   return vector;
 }
 
-lisp_object_t read_object(lisp_object_t port) {
+sexp read_object(sexp port) {
   int c = port_read_char(port);
   switch (c) {
     case EOF: return make_eof_object();
@@ -185,21 +186,22 @@ lisp_object_t read_object(lisp_object_t port) {
         case '\\': return read_character(port);
         case '(': return read_vector(port);
         default :
-          fprintf(stderr, "unexpected token '%c' at line %d\n", c, in_port_linum(port));
+          fprintf(stderr, "unexpected token '%c' at line %d\n",
+                  c, in_port_linum(port));
           exit(1);
       }
     }
     case '"': return read_string(port);
     case '(': {
-      lisp_object_t next = read_object(port);
+      sexp next = read_object(port);
       if (is_close_object(next))
-        return make_empty_list();
+        return EOL;
       else
         return make_pair(next, read_pair(port));
     }
     case ')': return make_close_object();
     case '\'': return make_pair(find_or_create_symbol("quote"),
-                                make_pair(read_object(port), make_empty_list()));
+                                make_pair(read_object(port), EOL));
     case '.': return make_dot_object();
     default : return read_symbol(c, port);
   }
